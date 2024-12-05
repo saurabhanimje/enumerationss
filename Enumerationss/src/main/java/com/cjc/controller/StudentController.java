8d38eaa4955c366067c9a349b61b7cce29d21e7f
saurabhanimje <nimjesaurabh1998@gmail.com>
2024-12-05 16:11:51 +0530

    by ss

--- Enumerationss/src/main/java/com/cjc/controller/StudentController.java
+++ Enumerationss/src/main/java/com/cjc/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.cjc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class StudentController
 	@Autowired
 	StudentService ss;
 	
+	@PostMapping("/post")
 	public ResponseEntity<Student> postData(@RequestBody Student student)
 	{
 		Student data=ss.postStudentData(student);
